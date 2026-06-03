801809763ec24089138f4aa2060726e2b3f6a9ca
Gabriel Botelho <gabriel.c.botelho@hotmail.com>
2026-06-03 16:20:56 -0300

    criando pagina de registro

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+
+
 function App() {
   return (
       <div className="App">
-        <Login />
+        <BrowserRouter>
+        <Routes>
+        <Route path= "/login" element={<Login/>}/>
+        <Route path ="/register" element={<Register/>}/>
+        </Routes>
+        </BrowserRouter>
       </div>
   )
 }
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,6 +1,7 @@
 import Button from "../../components/Button/Button";
 import InputField from "../../components/InputField/InputField";
 import styles from './Register.module.css';
+import stylesInput from '../../components/InputField/InputField.module.css';
 import { useState } from "react";
 import { FaUser, FaLock, FaEnvelope } from 'react-icons/fa';
 
@@ -15,7 +16,7 @@ const Register = () => {
  
     return (
     <div className={styles.container}>
-      <h1> Faça o Cadastro</h1>
+      <h1>Registre-se</h1>
 
       <form>
         <InputField
@@ -23,38 +24,41 @@ const Register = () => {
           placeholder="Nome Completo"
           value={name}
           onChange={(e) => setName(e.target.value)}
-          icon={<FaUser />}
+          icon={<FaUser className={stylesInput.icon} />}
         />
         <InputField
           type="text"
           placeholder="Nome de Usuário"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
-          icon={<FaUser />} 
+          icon={<FaUser className={stylesInput.icon} />}
         />
         <InputField
           type="email"
           placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          icon={<FaEnvelope />}
+          icon={<FaEnvelope className={stylesInput.icon} />}
         />
         <InputField
           type="password"
           placeholder="Senha"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          icon={<FaLock />}
+          icon={<FaLock className={stylesInput.icon} />}
         />
         <InputField
           type="password"
           placeholder="Confirmar Senha"
           value={confirmPassword}
           onChange={(e) => setConfirmPassword(e.target.value)}
-          icon={<FaLock />}
+          icon={<FaLock className={stylesInput.icon} />}
         />
         <Button type="submit">Registrar</Button>
       </form>
+      <div className={styles.signupLink}>
+                    <p>Já tem conta? <a href="#">Login</a></p>
+                  </div>
     </div>
   )}
 
